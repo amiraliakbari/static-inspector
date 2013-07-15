06801a467df69d638ffae3e2929c86c9b7c61d08
AmirAli Akbari <akbari@arsh.co>
2013-07-15 19:29:05 +0430

    basic modellings

--- inspector/models/base.py
+++ inspector/models/base.py
@@ -1,16 +1,49 @@
 # -*- coding: utf-8 -*-
+import os
+from inspector.models.consts import Language
 
 
 class Project(object):
-    pass
+    def __init__(self):
+        self.name = None
+        self.abs_path = None
 
 
 class Package(object):
-    pass
+    def __init__(self, project):
+        self.project = project
+        self.name = None
+
+    def get_abs_path(self):
+        return self.project.abs_path if self.project else None
 
 
 class File(object):
-    pass
+    def __init__(self):
+        self.file_content = None
+        self.filename = None
+
+    def get_abs_path(self):
+        return self.filename
+
+    def load_content(self):
+        with open(self.get_abs_path(), 'r') as f:
+            self.file_content = f.read()
+
+
+class SourceFile(File):
+    def __init__(self):
+        super(SourceFile, self).__init__()
+        self.language = Language.UNKNOWN
+        self.package = None
+
+    @property
+    def project(self):
+        return self.package.project
+
+    def get_abs_path(self):
+        pkg_abs = self.package.get_abs_path() if self.package else ''
+        return os.path.join(pkg_abs, self.filename)
 
 
 class Class(object):
---
+++ inspector/models/consts.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Language:
+    UNKNOWN = 0
+    JAVA = 1
+    PYTHON = 2
+
+
+class JavaFrameworks:
+    ANDROID = 11
+
+
+class PythonFrameworks:
+    DJANGO = 21
+    FLASK = 22

f7b8057c47d5e3af2a195d3e0e09a55f83f08cf1
AmirAli Akbari <amiraliakbari@gmail.com>
2013-12-26 19:01:20 +0330

    simple SAQL queries (SELECT classes)

--- inspector/saql/sams.py
+++ inspector/saql/sams.py
@@ -1,13 +1,46 @@
 # -*- coding: utf-8 -*-
+from inspector.models.android import AndroidProject
+from inspector.models.base import Method
 from inspector.saql.saql_parser import SaqlParser
 
 
 class SAMS(object):
+    QUERY_DEF = {
+        'SELECT': {
+            'class': ['file', 'project'],
+            'method': ['class', 'file', 'project'],
+            'file': ['project'],
+        },
+        'FUNCTIONS': {
+            'class': [
+                ('isSubClass', lambda c, parent: issubclass(c, parent)),
+            ],
+            'method': [
+                ('isAbstract', lambda m: m.abstract),
+                ('isPrivate', lambda m: m.access == Method.ACCESS.PRIVATE),
+                ('isProtected', lambda m: m.access == Method.ACCESS.PROTECTED),
+                ('isPackage', lambda m: m.access == Method.ACCESS.PACKAGE),
+                ('isPublic', lambda m: m.access == Method.ACCESS.PUBLIC),
+            ],
+        }
+    }
+
     def __init__(self):
         self.project = None
 
     def open_project(self, project_path):
-        self.project = project_path
+        self.project = AndroidProject(project_path)
+
+    def parse_identifier(self, identifier):
+        if identifier == ['project'] or identifier == 'project':
+            return self.project.all_files()
+        if isinstance(identifier, list) or isinstance(identifier, tuple):
+            return (self.project.find(iden) for iden in identifier)
+        return self.project.find(identifier)
+
+    def verify_query(self, query_def, query_type='SELECT'):
+        if not query_def[1] in self.QUERY_DEF[query_type][query_def[0]]:
+            raise ValueError('Query not applicable on these types: {0}'.format(query_def))
 
     def run_query(self, query):
         if not self.project:
@@ -15,7 +48,12 @@ class SAMS(object):
 
         q = SaqlParser.parse_query(query)
         if q.is_select_classes():
-            pass
+            classes = []
+            sel = ('class', q.select_from_type)
+            self.verify_query(sel)
+            for obj in self.parse_identifier(q.select_from):
+                classes += obj.classes
+            return classes
         elif q.is_select_methods():
             pass
         elif q.is_select_instances():
@@ -29,7 +67,7 @@ class SAMS(object):
     def run_action(self, action):
         if action.startswith(r'\c '):
             self.open_project(action[3:])
-            return
+            return "Project loaded"
         raise ValueError('Invalid Action: {0}'.format(action))
 
     def run(self, command):
--- inspector/test/saql_test.py
+++ inspector/test/saql_test.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
 import unittest
+from inspector.saql.sams import SAMS
 from inspector.saql.saql_parser import SaqlParser
 
 
@@ -21,3 +23,29 @@ class ParserTest(unittest.TestCase):
         self.assertListEqual(q1.select_from, ['project'])
         self.assertListEqual(q1.where_conditions, [])
         self.assertEqual(unicode(q1), qs1)
+
+
+class SAMSTest(unittest.TestCase):
+    def setUp(self):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', 'projects', 'gissue')
+        self.sams = SAMS()
+        self.sams.open_project(path)
+
+    def test_select_classes(self):
+        q1 = 'SELECT classes FROM project'
+        r = self.sams.run_query(q1)
+        self.assertItemsEqual([c.qualified_name for c in r],
+                              ['com.g.issue.IssueFragment', 'com.g.issue.IssuesFragment'])
+
+        q2 = 'SELECT classes FROM file:com.g.issue.IssueFragment'
+        r = self.sams.run_query(q2)
+        self.assertItemsEqual([c.qualified_name for c in r],
+                              ['com.g.issue.IssueFragment'])
+
+        q3 = 'SELECT classes FROM file:com.g.issue.IssueFragment, file:com.g.issue.IssuesFragment'
+        r = self.sams.run_query(q3)
+        self.assertItemsEqual([c.qualified_name for c in r],
+                              ['com.g.issue.IssueFragment', 'com.g.issue.IssuesFragment'])
+
+        q4 = 'SELECT classes FROM class:com.g.issue.IssueFragment'
+        self.assertRaises(ValueError, self.sams.run_query, q4)
